6aeaf0e377e08a42372e3e9560f3460dd84d6ef7
OmarElFakih <omarelfakih97@gmail.com>
2020-03-16 19:07:54 -0400

    placeholder visuals created

---
+++ CGProject/Assets/_Scripts/RingMarker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RingMarker : MonoBehaviour
+{
+    [SerializeField] private Image _ringImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_ringImage != null)
+        {
+            float _rotationAmount = _ringImage.fillAmount * -360;
+
+            Vector3 _rotation = new Vector3(0f, 0f, _rotationAmount);
+
+            transform.rotation = Quaternion.Euler(_rotation);
+        }
+    }
+}
